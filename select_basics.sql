c7a3e9f78c808dedb1c159fed1a21afd08d855cd
Henzi <mugunga5@gmail.com>
2020-05-20 13:49:44 +0200

    Add select_names file and shift query inside

--- select_basics.sql
+++ select_basics.sql
@@ -3,23 +3,3 @@ SELECT population FROM world WHERE name = 'Germany'
 SELECT name, population FROM world WHERE name IN ( 'Sweden', 'Norway' , 'Denmark');
 
 SELECT name, area FROM world WHERE area BETWEEN 200000 AND 250000
-
-SELECT name FROM world WHERE name LIKE 'Y%'
-
-SELECT name FROM world WHERE name LIKE '%y'
-
-SELECT name FROM world WHERE name LIKE '%x%'
-
-SELECT name FROM world WHERE name LIKE '%land'
-
-SELECT name FROM world WHERE name LIKE 'C%' AND name like '%ia'
-
-SELECT name FROM world WHERE name LIKE 'C%' AND name like '%ia'
-
-SELECT name FROM world WHERE name LIKE '%a%a%a%'
-
-SELECT name FROM world WHERE name LIKE '_t%' ORDER BY name
-
-SELECT name FROM world WHERE name LIKE '%o__o%'
-
-SELECT name FROM world WHERE name LIKE '____'
